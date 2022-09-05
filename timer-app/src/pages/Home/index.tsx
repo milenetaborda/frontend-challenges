355f54d29817286a7a696f72f705143639ee654f
Milene Taborda <milenetaborda1@outlook.com>
2022-09-05 14:26:23 -0300

    feat: adicionando estilo no formulário da home

---
+++ timer-app/src/pages/Home/index.tsx
@@ -0,0 +1,33 @@
+import { Play } from "phosphor-react";
+import * as S from "./styles";
+
+export const Home = () => {
+  return (
+    <S.HomeContainer>
+      <form action="#">
+        <S.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </S.FormContainer>
+
+        <S.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountdownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </S.HomeContainer>
+  );
+};
