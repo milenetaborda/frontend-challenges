ea4476fa2e9cfff488d70024235a3723becba1c6
Milene Taborda <milenetaborda1@outlook.com>
2022-09-05 14:25:58 -0300

    feat: adicionando estilo no header e no defaultLayout

--- timer-app/src/components/Header/index.tsx
+++ timer-app/src/components/Header/index.tsx
@@ -1,3 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+import { Scroll, Timer } from "phosphor-react";
+import logoImg from "../../assets/logo.svg";
+
+import * as S from "./styles";
+
 export const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <S.HeaderContainer>
+      <img src={logoImg} alt="" />
+
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </S.HeaderContainer>
+  );
 };
