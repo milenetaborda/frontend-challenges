582be61341d762197442453606aee5df3be5fa92
Milene Taborda <milenetaborda1@outlook.com>
2022-09-05 13:16:00 -0300

    feat: configuração dos estilos e rotas

---
+++ timer-app/src/styles/themes/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
+  }
+
+  body {
+    background: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
