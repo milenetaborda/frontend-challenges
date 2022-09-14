195847cf212c644191f2c788c2d91608198fff98
Milene Taborda <milenetaborda1@outlook.com>
2022-09-14 11:03:47 -0300

    feat: criação dos models Game e Ad

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -2,8 +2,22 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get("/games", (req: Request, res: Response) => {
+  res.status(200).json({ games: ["game1", "game2"] });
+});
+
+app.post("/ads", (req: Request, res: Response) => {
+  res.status(200).json([]);
+});
+
+app.get("/games/:id/ads", (req: Request, res: Response) => {
+  //const gameId = req.params.id;
+  res.status(200).send("Hello World!");
+});
+
+app.get("/games/:id/discord", (req: Request, res: Response) => {
+  //const adId = req.params.id;
+  res.status(200).send("Hello World!");
 });
 
 app.listen(3333, () => {
